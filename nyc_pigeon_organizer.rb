d112fbe9a2e610ad67a84789353375f9c705528b
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-23 23:59:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   
   data.each do |key, value|
-    value.each do key, array|
-      array_of_names =  array.collect do |element|
+    value.each do |key, array|
+      array_of_names = array.collect do |element|
         element
         end
         binding.pry
