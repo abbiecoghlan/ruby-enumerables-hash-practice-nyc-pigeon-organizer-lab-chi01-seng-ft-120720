ea7b516697ac97077b17cdd172745cf4fa64a6cf
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-23 23:29:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,9 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   
   data.each do |key, value|
-    
+    value.each do key, array|
+      
+    end 
   end
 
   ## iterate through the hash and collect each name(currently a value and also an array of names)
