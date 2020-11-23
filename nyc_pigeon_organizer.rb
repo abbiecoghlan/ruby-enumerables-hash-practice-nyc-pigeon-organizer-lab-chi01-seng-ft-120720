4ea9b80dbd6fab041afbe87defe4f057b8f93143
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-23 23:16:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  ## declare a new hash 
+
+  ## iterate through the hash and collect each name(currently a value and also an array of names)
+      # for each key, check the value array and IF it is not already in the list, add it
+
+  # insert each name as a the KEY of a new hash, whos value is an array of hashes with the keys :color, :gender, and :lives   
   
   pigeon_list = 
   
